644c685489b2d21a7bdee694432c1bd09d1354f3
Lara Salameh <hello@larasalameh.com>
2022-07-05 13:33:34 +1000

    first initial push live

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("hi");
+
+function codingBoss() {
+    // This is an alert for when the codingBoss function is called
+    // alert("hey you!");
+    document.querySelector('h1').style.color = "hotpink";
+}
+codingBoss();
