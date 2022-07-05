c56a0dc6435f7a68889b839306905be394ad0383
Lara Salameh <hello@larasalameh.com>
2022-07-05 13:41:13 +1000

    removed unneccessary code

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
 console.log("hi");
-
-function codingBoss() {
-    // This is an alert for when the codingBoss function is called
-    // alert("hey you!");
-    document.querySelector('h1').style.color = "hotpink";
-}
-codingBoss();
