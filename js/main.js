0385d789f2e6274f0fbf16d2d1eb54dcb3247410
Lara Salameh <hello@larasalameh.com>
2022-07-10 14:08:07 +1000

    lesson 2 code demos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log("hi");
+let firstPrize = 299;
+const companyName = "CLG";
+let greeting = "Hello!";
+let morning = true;
+
+// console.log(morning);
+// console.log(firstPrize);
+// console.clear();
+
+let firstWinner = "Grace";
+// console.log ("The winner is " + firstWinner);
+// console.log(`The winner is still ${firstWinner}`);
+
+let winnerAnnouncement = `The winner is really still ${firstWinner}`;
+
+let result = winnerAnnouncement.toUpperCase();
+
+// console.log(result);
+
+// console.log(`${result.length}`);
+
+let resultLength = result.length;
+console.log(resultLength);
